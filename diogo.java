9a2b25a8f46c59b9acbb94f5982d759f0366b58c
Diogo Leonardo <diogoleo57@gmail.com>
2023-03-22 16:37:45 -0300

    atualização

--- diogo.java
+++ diogo.java
@@ -3,9 +3,10 @@ package diogo;
 import java.util.Scanner;
 
 
-
 public class diogo {
 	public static void main (String[] args) {
+		//Algoritmo que permite a leitura de notas inseridas por usuários
+
 		Scanner scanner  = new Scanner(System.in);
 
 		System.out.println("Digite a primeira nota");
@@ -17,8 +18,10 @@ public class diogo {
 		System.out.println("Digite a terceira nota");
 		double nota3 = scanner.nextDouble();
 
+		//Algoritmo que estabelece a média das notas inseridas por usuários 
 		double media = (nota1 + nota2 + nota3) / 3;
 
+			//Algoritmo de acordo com média do usuário 
 		if (media > 10) {
 			System.out.println("Parabéns Você foi aprovado");
 		} else { 
